bb10342afd79dbd5669a4915bf8b208557c390cb
Ramin Esfandiari <ramin.esfan@gmail.com>
2023-01-25 15:25:30 +0000

    Pass failing test for Persian Saturday

--- src/main/scala/example/Demo.scala
+++ src/main/scala/example/Demo.scala
@@ -8,7 +8,7 @@ object Demo {
       case 3 => "Wednesday"
       case 4 => "Thursday"
       case 5 => "Friday"
-      case 6 => "Saturday"
+      case 6 => "Shanbeh"
       case 7 => "Sunday"
       case _ => "OutOfBounds"
 
