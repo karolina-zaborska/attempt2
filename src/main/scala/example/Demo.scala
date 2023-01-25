5d8a6bc6934cbb6c2434d39f23fb5dcb9d4079e4
Karolina <karolina.zaborska@testdrivensolutions.co.uk>
2023-01-25 15:05:26 +0000

    Add functionality for Poniedzialek

--- src/main/scala/example/Demo.scala
+++ src/main/scala/example/Demo.scala
@@ -3,7 +3,7 @@ package example
 object Demo {
 
     def weeklyFunction(number: Int): String = number match {
-      case 1 => "Monday"
+      case 1 => "Poniedzialek"
       case 2 => "Tuesday"
       case 3 => "Wednesday"
       case 4 => "Thursday"
--- src/test/scala/example/DemoTest.scala
+++ src/test/scala/example/DemoTest.scala
@@ -3,8 +3,8 @@ package example
 import org.scalatest.{FlatSpec, Matchers}
 
 class DemoTest extends FlatSpec with Matchers{
-  "The Demo object" should "say Monday" in {
-    Demo.weeklyFunction(1) shouldEqual "Monday"
+  "The Demo object" should "say Poniedzialek" in {
+    Demo.weeklyFunction(1) shouldEqual "Poniedzialek"
   }
 
   "The Demo object" should "say Tuesday" in {
